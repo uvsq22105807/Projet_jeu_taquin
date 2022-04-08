3458c1e54f7ef1bef3f6acbfddc6ce7801e037d1
uvsq22105807 <91261751+uvsq22105807@users.noreply.github.com>
2022-04-08 11:38:53 +0200

    Add files via upload

---
+++ jeutaquin.py
@@ -0,0 +1,34 @@
+import tkinter as tk  #librairie Tkinter
+from random import randint #choisir un nombre aléatoirement
+
+##################
+# Constantes
+
+LARGEUR = 400 #Car chaque ligne a 4 cases de 100px chacune
+HAUTEUR = 400 #Car il y a 4 lignes et chaque ligne fait 100px
+
+###################
+# Fonctions
+
+
+
+######################
+# programme principal
+
+# création des widgets
+racine = tk.Tk()
+canvas = tk.Canvas(racine, bg="black", width=LARGEUR, height=HAUTEUR)
+racine.title("Jeu du Taquin")
+
+
+# création des tuiles
+
+
+
+
+
+# déplacement des tuiles
+
+
+# boucle principale
+racine.mainloop()
