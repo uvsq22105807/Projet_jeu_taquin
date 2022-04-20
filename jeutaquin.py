d3481a6320a82fa16bb02d4056e9b1dddac9e50c
João Gonçalves Teixeira <joao.goncalves-teixeira@ens.uvsq.fr>
2022-04-20 12:05:12 +0200

    Boutons centrés et finis!

--- jeutaquin.py
+++ jeutaquin.py
@@ -29,7 +29,7 @@ def melanger():
 def sauvegarde():
     pass
 
-def retour():
+def annuler():
     mouvement = mouvement - 1
     pass
 
@@ -47,22 +47,23 @@ bouton_fermer = tk.Button(racine, text="Fermer",command=fermer_partie)
 bouton_charger = tk.Button(racine, text="Charger",command=charger_partie)
 bouton_melanger = tk.Button(racine, text="Melanger",command=melanger)
 bouton_sauvegarder= tk.Button(racine, text="Sauvegarder",command=sauvegarde)
-bouton_retour = tk.Button(racine, text="Annuler mouvement",command=retour)
+bouton_annuler = tk.Button(racine, text="Annuler mouvement",command=annuler)
 bouton_aide = tk.Button(racine, text="Aide",command=aide)
 
 #Positionnement des Widgets
 bouton_sauvegarder.grid(row=7, column=0, pady=2, padx=2,sticky=tk.W+tk.E, columnspan=1)
 bouton_charger.grid(row=7, column=1, pady=2, padx=2,sticky=tk.W+tk.E, columnspan=1)
-bouton_retour.grid(row=8, column=0, pady=2, padx=2,sticky=tk.W+tk.E, columnspan=1)
+bouton_annuler.grid(row=8, column=0, pady=2, padx=2,sticky=tk.W+tk.E, columnspan=1)
 bouton_aide.grid(row=8, column =1, pady=2, padx=2, sticky=tk.W+tk.E, columnspan=1)
 bouton_fermer.grid(row=10, column = 0, pady=2, padx=2, columnspan=2, sticky=tk.W+tk.E)
 bouton_melanger.grid(row=9, column=0, pady=2, padx=2, columnspan=2,sticky=tk.W+tk.E)
-
 canvas.grid(row = 0, column = 0, columnspan=2, sticky=tk.W+tk.E) #Tout le tableau de jeu est considéré comme une seule colonne, on doit alors l'étendre sur 2 colonnes pour pouvoir aligner les boutons en bas.
 
-# création des tuiles
-FONT=('Arial', 30, 'bold')
+racine.columnconfigure(1, minsize=200) #permet au canvas d'être centré avec les boutons
+racine.columnconfigure(0, minsize=200) #permet au canvas d'être centré avec les boutons
 
+# création des tuiles
+POLICE=('Arial', 30, 'bold')
 taquin=[[1, 2, 3, 4],
        [5, 6, 7, 8],
        [9, 10, 11, 12],
@@ -78,7 +79,7 @@ for i in range(4):
         A, B, C=(x, y), (x+100, y+100), (x+50, y+50)
         carre=canvas.create_rectangle(A, B, fill="gold")
         numero=taquin[i][j]
-        chiffre=canvas.create_text(C, text=numero, fill="black", font=FONT)
+        chiffre=canvas.create_text(C, text=numero, fill="black", font=POLICE)
         tuile[numero]=(carre, chiffre)
 canvas.delete(carre)
 canvas.delete(chiffre)
@@ -89,7 +90,7 @@ taquin_victoire=[[1, 2, 3, 4],
                 [9, 10, 11, 12],
                 [13, 14, 15, 16]]
 
-canvas.grid()
+
 # déplacement des tuiles
 def mouvement():
     pass
