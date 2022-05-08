d43a8e603f1ec7562b0fdbb8554d51c101f0ed88
João Gonçalves Teixeira <joao.goncalves-teixeira@ens.uvsq.fr>
2022-05-08 21:22:49 +0200

    Deplacement pas fini mais avancé

--- jeutaquin.py
+++ jeutaquin.py
@@ -1,3 +1,4 @@
+from ast import Pass
 import tkinter as tk  #librairie Tkinter
 import random #choisir un nombre aléatoirement
 from tkinter import messagebox #Ouvrir une pop-up avec une information
@@ -19,9 +20,27 @@ POLICE=('Arial', 30, 'bold') #Police d'écriture pour tout le jeu
 def identification(event):
     i=event.y//100 #On prends la cordonnée x et on divise par 100(taille de chaque tuile) on aura forcément un nombre compris entre 0 et 3
     j=event.x//100 #On prends la cordonnée y et on divise par 100(taille de chaque tuile) on aura forcément un nombre compris entre 0 et 3
+    i_vide=3 #carré vide est situé en i=3 taquin[3][3]
+    j_vide=3 #carré vide est situé en j=3 taquin[3][3]
     print("Ligne :", i, "Colonne :", j,"Tuile n° :", taquin[i][j]) #On va verifier dans taquin à quel numero ça correspond.
+    if i-1==i_vide and j==j_vide:
+        taquin[i][j],taquin[i_vide][j_vide]=(taquin[i_vide][j_vide],taquin[i][j])
+        deplacer()
+        tableaudejeu()
+    elif i+1==i_vide and j==j_vide:
+        taquin[i][j],taquin[i_vide][j_vide]=(taquin[i_vide][j_vide],taquin[i][j])
+        deplacer()
+        tableaudejeu()
+    elif j-1==j_vide and i==i_vide:
+        taquin[i][j],taquin[i_vide][j_vide]=(taquin[i_vide][j_vide],taquin[i][j])
+        deplacer()
+        tableaudejeu()
+    elif j+1==j_vide and i==i_vide:
+        taquin[i][j],taquin[i_vide][j_vide]=(taquin[i_vide][j_vide],taquin[i][j])
+        deplacer()
+        tableaudejeu()
     
-    deplacer()
+
 
 # déplacement des tuiles
 def deplacer():
@@ -30,7 +49,6 @@ def deplacer():
         messagebox.showinfo("Taquin","Bravo, tu as gagné!")
         print("Tu as gagné en", mouvement, "coups.")
     else : 
-
         for i in range(4): 
             for j in range(4):
                 taquinprecedent.append(taquin[i][j]) 
@@ -104,11 +122,14 @@ def charger_partie():
 
 
 def annuler():
-    global taquin, taquinprecedent
-    for i in range(4):
-        for j in range(4):
-            taquinprecedent[i][j]=taquin[i][j]
-            mouvement=mouvement-1
+    global taquin, taquinprecedent,mouvement
+    if mouvement>0:
+        for i in range(4):
+            for j in range(4):
+                taquinprecedent[i][j]=taquin[i][j]
+                mouvement=mouvement-1
+    else:
+        messagebox.showerror("Annuler Mouvement", "Vous n'avez pas de mouvement pouvant être annulé.")
     tableaudejeu()  #Redessine le tableau avec la nouvelle matrice, actualise le tableau. 
     
 
@@ -121,13 +142,13 @@ def aide():
 ###############################################################################################
 ###############################################################################################
 
-# programme principal
+# Matrices
 
 taquin=[[1, 2, 3, 4],
        [5, 6, 7, 8],
        [9, 10, 11, 12],
        [13, 14, 15, 16]]
-#Les numeros sur les tuiles suivront cette matrice. Matrice qui doit être melangée.
+#Les numeros sur les tuiles suivront cette matrice. Matrice qui est melangée à l'execution du code.
 
 taquinprecedent=[None for x in range(100)] 
 #On crée 100 listes vides, taquinprecedent[mouvement] representera la matrice apres chaque coup.
